d9913076c83326a920c7e94cdf4c92d17e0212fa
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-10 13:58:01 +0300

    added index

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,17 +5,17 @@
 #	- service restart
 exec { '/usr/bin/env apt-get -y update':}
 -> package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 -> file { '/var/www/html/index.html':
   content => 'Holberton School!',
 }
 -> file_line { 'add header':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By ${HOSTNAME};",
   after  => 'server_name _;',
 }
 -> service { 'nginx':
-  ensure => 'running',
+  ensure => running,
 }
